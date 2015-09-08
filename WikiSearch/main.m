de735376054de5ca2ea10c71114c4cc9223e788f
Astha Trivedi <astha_trivedi@intuit.com>
2015-09-07 23:16:42 -0700

    Initial Commit

---
+++ WikiSearch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WikiSearch
+//
+//  Created by Trivedi, Astha on 07/09/15.
+//  Copyright (c) 2015 Astha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
